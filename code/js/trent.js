cbedcf6d82b47ad5aec71e7f7b8bf813cbaa2896
tmyee <trent_yee@brown.edu>
2016-03-15 18:23:27 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -3,11 +3,40 @@ $(document).ready(function() {
     width = 960 - margin.left - margin.right
     height = 430 - margin.top - margin.bottom
     categories = ["Environment", "Games", "Fashion", "Technology", "Sports"];
-   
+    
+  var x = d3.scale.ordinal()
+    .range([0, width])
+    .domain(categories);
+    
+  var y = d3.scale.linear()
+    .range([0, height]);
+    
+  var selections = ["Funding", "Views"], 
+    j = 0;  // Choose the rectangle as default
+
+// Create the shape selectors
+var form = d3.select("#trent").append("form");
+
+labels = form.selectAll("label")
+    .data(selections)
+    .enter()
+    .append("label")
+    .text(function(d) {return d;})
+    .insert("input")
+    .attr({
+        type: "radio",
+        class: "selection",
+        name: "mode",
+        value: function(d, i) {return i;}
+    })
+    .property("checked", function(d, i) {return i===j;});
+    
+    
   d3.json("https://raw.githubusercontent.com/alexwainger/viz2/master/code/data/data.json", function(error, d) {
     var data = d.data;
     console.log(data);
     console.log(data.length);
+    
     //console.log(data);
   });
 });
