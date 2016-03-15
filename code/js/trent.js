9575e5cfb78023724e0b791770c0b01160ad989e
tmyee <trent_yee@brown.edu>
2016-03-15 18:30:09 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
   // Create the shape selectors
   var form = d3.select("#trent").append("form");
   console.log("RADIO");
-  labels = form.selectAll("label")
+  labels = form
     .data(selections)
     .enter()
     .append("label")
