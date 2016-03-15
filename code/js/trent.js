35b1f8a320ee0a76ca1fc1b8aab7713461f06bd1
tmyee <trent_yee@brown.edu>
2016-03-15 17:43:34 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-  d3.json("../data/data.json", function(d) {
-    var data = d.data;
-    for(var i = 0; i < data.length; i ++) {
-      console.log(data[i].gender);
-    }
+  var margin = {top: 50, right: 0, bottom: 100, left: 30},
+    width = 960 - margin.left - margin.right
+    height = 430 - margin.top - margin.bottom
+    categories = ["Environment", "Games", "Fashion", "Technology", "Sports"];
+   
+  
+  d3.json("../data/data.json", function(error, data) {
+    data.forEach(function(d) {
+      console.log(d);
+    });
   });
 });
   
