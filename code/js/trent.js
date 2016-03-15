4512a525834c2a4e2c23fc83953fa1929f426074
tmyee <trent_yee@brown.edu>
2016-03-15 19:28:01 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
     
   svg.append("g")
     .attr("class", "x_axis")
+    .attr("transform", "translate(0," + height + ")");
     .call(xAxis);
     
   svg.append("g")
