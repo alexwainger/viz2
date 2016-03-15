da888cd7aef0384829254f1d94195627563f80df
tmyee <trent_yee@brown.edu>
2016-03-15 17:31:53 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -1,1 +1,9 @@
-d3.select.("body").select("#trent").append("p").text("Bar Graph");
+$(document).ready(function() {
+  d3.json("../data/data.json", function(d) {
+    var data = d.data;
+    for(var i = 0; i < data.length; i ++) {
+      console.log(data[i]);
+    }
+  });
+});
+  
