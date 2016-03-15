bb8fbf90d4ab0c47181cb472111dfb5bdd6dd19b
tmyee <trent_yee@brown.edu>
2016-03-15 19:24:51 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
     .domain(categories);
     
   var y = d3.scale.linear()
-    .range([0, height])
-    .domain([100, 0]);
+    .range([height, 0])
+    .domain([0, 100]);
   
   var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("top");
+    .orient("bottom");
     
   var yAxis = d3.svg.axis()
     .scale(y)
