360b96e24546c266d833b736fd3d74c2ae529ee5
tmyee <trent_yee@brown.edu>
2016-03-15 17:34:26 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   d3.json("../data/data.json", function(d) {
     var data = d.data;
     for(var i = 0; i < data.length; i ++) {
-      console.log(data[i]);
+      console.log(data[i].gender);
     }
   });
 });
