0ad957debd4c58958040120e70959733af233a91
tmyee <trent_yee@brown.edu>
2016-03-15 22:36:19 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var margin = {top: 50, right: 0, bottom: 100, left: 80},
+  var margin = {top: 25, right: 0, bottom: 100, left: 80},
     width = 960 - margin.left - margin.right
     height = 430 - margin.top - margin.bottom
     categories = ["Environment", "Games", "Fashion", "Technology", "Sports"]
