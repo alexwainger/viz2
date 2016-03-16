81d3c65ffe8f279a9bc29b82d35447c70fb1281c
tmyee <trent_yee@brown.edu>
2016-03-15 20:47:31 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   var margin = {top: 50, right: 0, bottom: 100, left: 30},
     width = 960 - margin.left - margin.right
     height = 430 - margin.top - margin.bottom
-    categories = ["Environment", "Games", "Fashion", "Technology", "Sports"];
+    categories = ["Environment", "Games", "Fashion", "Technology", "Sports"]
+    color_bucket = ['#000099', '#990099']; // color_bucket[0]: male; color_bucket[1]: female
   
   var x = d3.scale.ordinal()
     .rangeBands([width, 0])
@@ -50,13 +51,14 @@ $(document).ready(function() {
     .insert("input")
     .attr({
       type: "radio",
-      class: "selection",
+      id: "selection",
       name: "mode",
       value: function(d, i) {return i;}
     })
     .property("checked", function(d, i) {return i===j;});
     
-    
+  var choice = document.getElementById("selection");
+  console.log(choice);
   d3.json("https://raw.githubusercontent.com/alexwainger/viz2/master/code/data/data.json", function(error, d) {
     var data = d.data;
     console.log(data);
