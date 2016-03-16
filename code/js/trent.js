b8ca9ae52ff9865c059d6ec828b0f1b75617d4ec
tmyee <trent_yee@brown.edu>
2016-03-16 02:16:19 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -279,12 +279,12 @@ $(document).ready(function() {
     .attr("y", 0)
     .attr("height", y(5000))
     .style("fill", function(d) { return color_bucket[0]; });*/
-  state.selectAll("rect")
+  state.select("rect")
     .data(m_funding).enter().append("rect")
     .attr("width", "18px")
     .attr("height", function(d) { return (height - y(d)) + "px"; })
     .attr("x", 0)
-    .attr("y", function(d) { return (height - y(d)) + "px"; })
+    .attr("y", function(d) { return y(d) + "px"; })
     .style("fill", function(d) { return color_bucket[0]; });
     
 
