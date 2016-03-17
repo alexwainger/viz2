bc8f00c42b0e240591ea6ab41a7ace5f5c7b324f
tmyee <trent_yee@brown.edu>
2016-03-16 22:11:33 -0400

    Update trent.js

--- code/js/trent.js
+++ code/js/trent.js
@@ -470,7 +470,8 @@ $(document).ready(function() {
         .attr("x", width - 18)
         .attr("width", 18)
         .attr("height", 18)
-        .style("fill", function() { return color_bucket[i]; })
+        .style("fill", function() { return color_bucket[i]; });
+        
       d3.select(this).append("text")
         .attr("x", width - 24)
         .attr("y", 9)
